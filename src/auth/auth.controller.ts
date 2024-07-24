71d3bf4706d196717e9cc79b32ace73845846f50
SrishBansal <besrish@gmail.com>
2024-07-25 00:48:07 +0530

    deleted auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,4 +13,8 @@ export class AuthController {
     return this.authService.register(registerData);
   }
 
+  @Get()
+  login(@Body() loginData: LoginDto){
+    return this.authService.login(loginData)
+  }
 }
