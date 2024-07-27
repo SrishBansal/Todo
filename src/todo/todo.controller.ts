68edc6755102d636e4b8cacaaff34749cd314dcc
SrishBansal <besrish@gmail.com>
2024-07-27 17:38:03 +0530

    "controller channges"

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { JwtAuthGuard } from '../auth/auth.gard'; 
-import { UserEmail } from 'src/common/decorator/user-email.decorator';
+import { JwtAuthGuard } from '../auth/auth.guard';
+import { UserEmail } from '../common/decorators/user-email.decorator';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+
 
 @ApiTags('Todo')
 @Controller('todo')
@@ -12,8 +13,8 @@ export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard) 
-  @ApiOperation({description: "To add a new Todo.", summary:"Start new todo."})
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description:'To Add a new task wrt to the user email.', summary: 'Add a new Task.' })
   @Post()
   async create(@Body() createTodoDto: CreateTodoDto, @UserEmail()
   userEmail: string) {
@@ -23,35 +24,35 @@ export class TodoController {
 
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @ApiOperation({description: "To get all user task.", summary:"Get user task"})
+  @ApiOperation({ description:'To get all the user tasks.', summary: 'To get all the user tasks.' })
+  @Get()
   async findAll(@UserEmail()
-userEmail: string) {
+  userEmail: string) {
     console.log(userEmail)
-    return this.todoService.findAll(userEmail);
+    return await this.todoService.findAll(userEmail);
   }
 
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @ApiOperation({description: "To get specific user task.", summary:"Get specific task"})
+  @ApiOperation({ description:'To get a specific the user task.', summary: 'To get a specific the user task.' })
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<{ id: number; task: string; description: string | null; status: import(".prisma/client").$Enums.TodoStatus; createdAt: Date; updatedAt: Date; }> {
+  findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
   }
 
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description:'To update a specific the user task.', summary: 'To update a specific the user task.' })
   @Patch(':id')
-  @ApiOperation({description: "To update specific user task.", summary:"update user task"})
-  async update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(+id, updateTodoDto);
   }
-  
+
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description:'To delete a specific the user task.', summary: 'To delete a specific the user task.' })
   @Delete(':id')
-  @ApiOperation({description: "To delete  user task.", summary:"Delete user task"})
-  async remove(@Param('id') id: string) {
+  remove(@Param('id') id: string) {
     return this.todoService.remove(+id);
   }
 }
-
